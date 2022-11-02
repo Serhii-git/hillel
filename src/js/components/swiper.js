a65771b338d6a1b772a7b62d09c3e48cbd39da36
Serhii_Retsenko <s.s.retsenko@gmail.com>
2022-11-02 16:29:55 +0200

    finished_index.html

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,15 +1,17 @@
 import Swiper, { Pagination, Autoplay } from 'swiper';
 Swiper.use([Pagination, Autoplay]);
 const swiper = new Swiper('.team-swiper', {
-  // autoplay: {
-  //   delay: 3000,
-  // },
+  autoplay: {
+    delay: 3000,
+  },
   slidesPerView: '1',
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
-    dynamicBullets: true,
-    dynamicMainBullets: 1,
+
+    // dynamicBullets: true,
+    // dynamicMainBullets: 1,
   },
 });
